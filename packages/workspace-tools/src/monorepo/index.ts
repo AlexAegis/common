837973555e6e3edecd550bebabb99d1657cdcae5
AlexAegis <alexaegis@gmail.com>
2023-01-31 01:15:06 +0100

    fix: add missing options export

--- packages/workspace-tools/src/monorepo/index.ts
+++ packages/workspace-tools/src/monorepo/index.ts
@@ -2,6 +2,7 @@ export * from './collect-workspace-packages.function.js';
 export * from './collect-workspace-packages.function.options.js';
 export * from './collect-workspace-packages.function.yargs.js';
 export * from './distribute-file-in-workspace.function.js';
+export * from './distribute-file-in-workspace.function.options.js';
 export * from './distribute-file-in-workspace.function.yargs.js';
 export * from './distribute-in-workspace.options.js';
 export * from './distribute-package-json-items-in-workspace.function.js';
