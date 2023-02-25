1faa3feccab1fdf16adc5757f75f94c8e9288ca7
AlexAegis <alexaegis@gmail.com>
2023-02-26 00:28:37 +0100

    feat(common): added the safe option

--- packages/common/src/options/index.ts
+++ packages/common/src/options/index.ts
@@ -2,3 +2,4 @@ export * from '../functions/noop.function.js';
 export * from './dry.function.js';
 export * from './dry.option.js';
 export * from './force.option.js';
+export * from './safe.option.js';
