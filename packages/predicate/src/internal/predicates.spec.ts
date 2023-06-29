2caf5f57e0cdef32f1e3bfba377a4ede94655d47
AlexAegis <alexaegis@gmail.com>
2023-06-29 23:32:51 +0200

    feat(predicate): allow undefined

--- packages/predicate/src/internal/predicates.spec.ts
+++ packages/predicate/src/internal/predicates.spec.ts
@@ -3,80 +3,105 @@ import { and, contains, equal, matchRegExp, not, or } from './predicates.js';
 
 describe('predicates', () => {
 	describe('equal', () => {
+		const assertion = equal('foo');
+
 		it('should create a predicate that return true for equality', () => {
-			const assertion = equal('foo');
 			expect(assertion('foo')).toBeTruthy();
 		});
 
 		it('should create a predicate that return false when it does not equal', () => {
-			const assertion = equal('foo');
 			expect(assertion('bar')).toBeFalsy();
+		});
+
+		it('should return false when matching with undefined', () => {
+			expect(assertion(undefined)).toBeFalsy();
 		});
 	});
 
 	describe('contains', () => {
+		const assertion = contains('f');
+
 		it('should create a predicate that returns true when includes returns true', () => {
-			const assertion = contains('f');
 			expect(assertion('foo')).toBeTruthy();
 		});
 
 		it('should create a predicate that returns false when the value does not include the criteria', () => {
-			const assertion = contains('f');
 			expect(assertion('bar')).toBeFalsy();
+		});
+
+		it('should return false when matching with undefined', () => {
+			expect(assertion(undefined)).toBeFalsy();
 		});
 	});
 
 	describe('not', () => {
+		const assertion = not(equal('foo'));
+
 		it('should negate other operators, a matching equal should return false', () => {
-			const assertion = not(equal('foo'));
 			expect(assertion('foo')).toBeFalsy();
 		});
 
 		it('should negate other operators, a non matching equal should return true', () => {
-			const assertion = not(equal('foo'));
 			expect(assertion('bar')).toBeTruthy();
+		});
+
+		it('should return true when matching with undefined because it is negated', () => {
+			expect(assertion(undefined)).toBeTruthy();
 		});
 	});
 
 	describe('and', () => {
+		const assertion = and(equal('foo'), contains('fo'));
+
 		it('should and together other operators, return true only when all matches', () => {
-			const assertion = and(equal('foo'), contains('fo'));
 			expect(assertion('foo')).toBeTruthy();
 		});
 
 		it('should and together other operators, return false only when any fails to match', () => {
-			const assertion = and(equal('bar'), contains('fo'));
 			expect(assertion('bar')).toBeFalsy();
+		});
+
+		it('should return false when matching with undefined', () => {
+			expect(assertion(undefined)).toBeFalsy();
 		});
 	});
 
 	describe('or', () => {
+		const assertion = or(equal('null'), contains('fo'));
+
 		it('should or together other operators, return true when any matches', () => {
-			const assertion = or(equal('null'), contains('fo'));
 			expect(assertion('foo')).toBeTruthy();
 		});
 
 		it('should or together other operators, return false when none matches', () => {
-			const assertion = or(equal('null'), contains('fo'));
 			expect(assertion('bar')).toBeFalsy();
+		});
+
+		it('should return false when matching with undefined', () => {
+			expect(assertion(undefined)).toBeFalsy();
 		});
 	});
 
 	describe('matchRegExp', () => {
+		const assertion = matchRegExp(/fo.*/);
+
 		it('should return true when the criteria regexp matches', () => {
-			const assertion = matchRegExp(/fo.*/);
 			expect(assertion('foo')).toBeTruthy();
 		});
 
 		it('should return false when the criteria regexp does not match', () => {
-			const assertion = matchRegExp(/fo.*/);
 			expect(assertion('bar')).toBeFalsy();
+		});
+
+		it('should return false when matching with undefined', () => {
+			expect(assertion(undefined)).toBeFalsy();
 		});
 	});
 
-	describe('more complex usecases', () => {
+	describe('a more complex usecase', () => {
+		const predicate = or(and(contains('fo'), contains('oo')), equal('bar'));
+
 		it('should be able to nest predicates deeply', () => {
-			const predicate = or(and(contains('fo'), contains('oo')), equal('bar'));
 			expect(predicate('foo')).toBeTruthy();
 			expect(predicate('bar')).toBeTruthy();
 			expect(predicate('zed')).toBeFalsy();
--- packages/predicate/src/internal/predicates.ts
+++ packages/predicate/src/internal/predicates.ts
@@ -1,27 +1,28 @@
-export type Predicate<T> = (value: T) => boolean;
+export type Predicate<T> = (value?: T) => boolean;
 
 export const not = <T>(predicate: Predicate<T>): Predicate<T> => {
-	return (value: T): boolean => !predicate(value);
+	return (value?: T): boolean => !predicate(value);
 };
 
 export const equal = <T>(criteria: T) => {
-	return (value: T): value is T => value === criteria;
+	return (value?: T): value is T => value === criteria;
 };
 
 export const and = <T>(...predicates: Predicate<T>[]): Predicate<T> => {
-	return (value: T): boolean => predicates.every((predicate) => predicate(value));
+	return (value?: T): boolean => predicates.every((predicate) => predicate(value));
 };
 
 export const or = <T>(...predicates: Predicate<T>[]): Predicate<T> => {
-	return (value: T): boolean => predicates.some((predicate) => predicate(value));
+	return (value?: T): boolean => predicates.some((predicate) => predicate(value));
 };
 
 export const contains = <T extends string>(criteria: T): Predicate<string> => {
-	return (value: T | string): value is string => value.includes(criteria);
+	return (value?: T | string): value is string => value?.includes(criteria) ?? false;
 };
 
 export const matchRegExp = <T extends string>(criteria: RegExp): Predicate<string> => {
-	return (value: T | string): value is string => criteria.test(value);
+	return (value?: T | string): value is string =>
+		typeof value === 'string' ? criteria.test(value) : false;
 };
 
 export const predicate = {
