ce69edcdf273c9b214ad1d648ef3f2586051eb2f
AlexAegis <alexaegis@gmail.com>
2023-12-28 17:16:37 +0100

    chore: updated packages

--- packages/cli-tools/src/yargs/dry.yargs.spec.ts
+++ packages/cli-tools/src/yargs/dry.yargs.spec.ts
@@ -5,7 +5,7 @@ import { yargsForDryOption } from './dry.yargs.js';
 describe('yargsForDryOption', () => {
 	it('should parse', async () => {
 		const dryness = true;
-		const yarguments = yargsForDryOption(yargs(['--dry', `${Boolean(dryness).toString()}`]));
+		const yarguments = yargsForDryOption(yargs(['--dry', Boolean(dryness).toString()]));
 		const args = await yarguments.parseAsync();
 
 		expect(args.dry).toEqual(dryness);
