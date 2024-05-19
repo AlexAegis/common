78247b19ea31a7e4ff7719e21a12086d8e980fcf
AlexAegis <alexaegis@pm.me>
2024-05-20 01:02:38 +0200

    feat(common): added loosened branded types

--- packages/common/src/objects/brand.type.ts
+++ packages/common/src/objects/brand.type.ts
@@ -2,3 +2,7 @@ declare const __brand: unique symbol;
 type Brand<B> = { [__brand]: B };
 
 export type Branded<T, B> = T & Brand<B>;
+export type LoosenBrand<T extends Branded<unknown, unknown>> = Omit<T, typeof __brand> & {
+	[__brand]?: T[typeof __brand];
+};
+export type BrandedLoosely<T, B> = LoosenBrand<Branded<T, B>>;
